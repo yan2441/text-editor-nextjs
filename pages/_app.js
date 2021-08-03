f578a4ea2ba8a4a3dddc5733b7d98dbb91cc7f08
Yassine najem <yasen2441@gmail.com>
2021-08-03 19:04:30 +0100

    clean up the project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import 'tailwindcss/tailwind.css'
+import "@material-tailwind/react/tailwind.css";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
