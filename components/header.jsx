8a832252502cdbbcd6929f8ea402c875abf4b884
Yassine najem <yasen2441@gmail.com>
2021-08-08 21:17:48 +0100

    fix some responsive issues

--- components/header.jsx
+++ components/header.jsx
@@ -19,10 +19,10 @@ function Header() {
         <Icon name="menu" size="3xl" />
       </Button>
       <Icon name="description" size="3xl" color="blue" />
-      <h1 className="ml-2 text-2xl text-gray-700 ">Docs</h1>
+      <h1 className="hidden ml-2 text-2xl text-gray-700 md:inline-flex ">Docs</h1>
 
       {/* search bar */}
-      <div className="flex items-center flex-grow px-5 py-2 mx-5 text-gray-600 bg-gray-100 rounded-lg focus-within:text-gray-600 focus-within:shadow-md">
+      <div className="flex items-center flex-grow px-5 py-2 mx-3 text-gray-600 bg-gray-100 rounded-lg focus-within:text-gray-600 focus-within:shadow-md">
         <Icon name="search" size="3xl" color="gray" />
         <input type="text" placeholder="Search" className="flex flex-grow px-5 text-base bg-transparent outline-none" />
       </div>
@@ -33,7 +33,7 @@ function Header() {
         rounded={true}
         iconOnly={true}
         ripple="dark"
-        className="w-20 h-20 ml-5 border-0 md:inline-flex md:ml-20"
+        className="hidden w-20 h-20 ml-5 border-0 md:inline-flex md:ml-20"
       >
         <Icon name="apps" size="3xl" color="gray" />
       </Button>
