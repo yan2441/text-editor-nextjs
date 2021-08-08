667f67e83d92ff283faaecd2363d6b3e06251589
Yassine najem <yasen2441@gmail.com>
2021-08-08 21:03:28 +0100

    update logo ref

--- components/login.jsx
+++ components/login.jsx
@@ -7,7 +7,7 @@ function Login() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Image
-        src="https://links.papareact.com/1ui"
+        src="/logo.png"
         height="300"
         width="550"
         objectFit="contain"
